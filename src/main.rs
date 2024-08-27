912806e21d97520988bf6fc59d935ef5b8f37cd3
Luke Lynch <luke@lukelynch.io>
2024-08-28 00:04:14 +1000

    Added some notes to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+     *
+     * 1. Check for crontab job
+     * 2. Check for local share database ( sort it into 1mb files... or into maybe months)
+     * 3. Check for Today's Date
+     * 4. Check for missing entries
+     * 5. If there are missing entries. ask to fill out, mark all as in office/wfh/on leave/exemption/ignore
+     * 6. Ask what today's entry is. (office/wfh/on leave/exemption/ignore)
+     * 7. maybe ask if they want a report? or maybe leave it for a separate menu
+     *
+     * executable flag to skip report question??
+     */
 }

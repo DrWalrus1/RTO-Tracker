d965dbd774d27ce05a7d144bc06dbaef98810e64
Luke Lynch <luke@lukelynch.io>
2024-08-28 00:20:03 +1000

    Added test run of flag

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::env;
+
+const SKIP_REPORT_FLAG: &str = "--skip-report-flag";
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut skip_report: bool = false;
+
+    for i in 1..args.len() {
+        match args[i].as_str() {
+            SKIP_REPORT_FLAG => skip_report = true,
+            _ => {}
+        }
+    }
+    println!("Skip report flag is detected: {}", skip_report);
     /*
      *
      * 1. Check for crontab job
